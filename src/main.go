d3193cc7e1a3341877a2122f43fda6941e4d63f8
Gabriel Cruz <gabs.oficial98@gmail.com>
2019-11-24 14:03:03 -0300

    Add conversion (normal to centh, centh to normal)

--- src/main.go
+++ src/main.go
@@ -21,8 +21,29 @@ type centhTime struct {
 	centhours  int
 }
 
-func toCenth(normal normalTime) centhTime {
-	return centhTime{}
+// Default value of cs (in seconds)
+var csVal float = 0.35999997
+
+// Default value of ct (in seconds)
+var ctVal float = 35.999997
+
+// Default value of ch (in seconds)
+var chVal float = 3599.9997
+
+func toCenth(n normalTime) centhTime {
+	return centhTime{
+		n.seconds * csVal,
+		n.minutes * ctVal,
+		n.hours * chVal,
+	}
+}
+
+func toNormal(c centhTime) normalTime {
+	return normalTime{
+		c.centhconds / csVal,
+		c.centhutes / ctVal,
+		c.centhours / chVal,
+	}
 }
 
 func main() {
@@ -30,7 +51,7 @@ func main() {
 
 	app.Name = "cclock"
 	app.Usage = "cclock's command-line interface"
-	app.Version = "0.1"
+	app.Version = "0.2"
 
 	app.Action = func(c *cli.Context) error {
 		n := normalTime{}
