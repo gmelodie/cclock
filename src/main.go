95b2a97ac43d0cade35d99f1ec21e23f0588658b
Gabriel Cruz <gabs.oficial98@gmail.com>
2019-11-24 14:15:58 -0300

    First working version (bugs)

--- src/main.go
+++ src/main.go
@@ -10,25 +10,25 @@ import (
 )
 
 type normalTime struct {
-	seconds int
-	minutes int
-	hours   int
+	seconds float64
+	minutes float64
+	hours   float64
 }
 
 type centhTime struct {
-	centhconds int
-	centhutes  int
-	centhours  int
+	centhconds float64
+	centhutes  float64
+	centhours  float64
 }
 
 // Default value of cs (in seconds)
-var csVal float = 0.35999997
+var csVal = 0.35999997
 
 // Default value of ct (in seconds)
-var ctVal float = 35.999997
+var ctVal = 35.999997
 
 // Default value of ch (in seconds)
-var chVal float = 3599.9997
+var chVal = 3599.9997
 
 func toCenth(n normalTime) centhTime {
 	return centhTime{
@@ -51,7 +51,7 @@ func main() {
 
 	app.Name = "cclock"
 	app.Usage = "cclock's command-line interface"
-	app.Version = "0.2"
+	app.Version = "0.3"
 
 	app.Action = func(c *cli.Context) error {
 		n := normalTime{}
@@ -63,16 +63,18 @@ func main() {
 		}
 
 		if c.NArg() >= 1 {
-			n.seconds, _ = strconv.Atoi(c.Args().Get(0))
+			n.seconds, _ = strconv.ParseFloat(c.Args().Get(0), 32)
 		}
 		if c.NArg() >= 2 {
-			n.minutes, _ = strconv.Atoi(c.Args().Get(1))
+			n.minutes, _ = strconv.ParseFloat(c.Args().Get(1), 32)
 		}
 		if c.NArg() == 3 {
-			n.hours, _ = strconv.Atoi(c.Args().Get(2))
+			n.hours, _ = strconv.ParseFloat(c.Args().Get(2), 32)
 		}
 
-		fmt.Println(n)
+		centh := toCenth(n)
+		fmt.Printf("Sleeping for\t%fch\t%fct\t%fcs\n", centh.centhours, centh.centhutes, centh.centhconds)
+
 		return nil
 	}
 
